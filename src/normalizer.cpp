939e02c0de9980845f320962680296ed196a6375
trungtq <taquangtrungvn@gmail.com>
2020-07-07 21:41:21 +0800

    temporarily disable some features

--- src/normalizer.cpp
+++ src/normalizer.cpp
@@ -1,54 +1,67 @@
 #include <iostream>
 
-#include "llvm-c/Core.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/Metadata.h"
-#include "llvm/IR/Function.h"
-#include "llvm/IR/Constants.h"
-#include "llvm/IR/LLVMContext.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/DebugInfoMetadata.h"
 #include "llvm/IRReader/IRReader.h"
+
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/Support/Debug.h"
 #include "llvm/Support/SourceMgr.h"
-#include "llvm/Support/Casting.h"
 #include "llvm/Support/raw_ostream.h"
+
+#include "llvm/Passes/PassBuilder.h"
+
+// #include "NormalizeConstExpr.h"
+
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/PassManager.h"
+
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
 
 using namespace std;
 using namespace llvm;
 
-void preprocess_constant_expr(llvm::Module* module) {
-  for (auto it = module->getFunctionList().begin();
-       it != module->getFunctionList().end();
-       ++it) {
+using FunctionListType = SymbolTableList<Function>;
+using BasicBlockListType = SymbolTableList<BasicBlock>;
+
+
+bool normalizeModule(Module& module) {
+  FunctionListType &funcList = module.getFunctionList();
+
+  for (auto it = funcList.begin(); it != funcList.end(); ++it) {
     auto func = it;
+    BasicBlockListType &blockList = func->getBasicBlockList();
 
-    for (auto it2 = func->getBasicBlockList().begin();
-         it2 != func->getBasicBlockList().end();
-         ++it2) {
-      BasicBlock* blk = &(*it2);
-
+    for (auto it2 = blockList.begin(); it2 != blockList.end(); ++it2) {
+      BasicBlock *blk = &(*it2);
       IRBuilder<> builder(blk);
 
       for (auto it3 = blk->begin(); it3 != blk->end(); ++it3) {
-        Instruction* instr = &(*it3);
-
+        Instruction *instr = &(*it3);
         builder.SetInsertPoint(instr);
 
-        for (int i = 0; i < instr->getNumOperands(); i++) {
-          Value* operand = instr->getOperand(i);
-          if (ConstantExpr* expr = dyn_cast<ConstantExpr>(operand)) {
-            Instruction* exprInstr = expr->getAsInstruction();
-            builder.Insert(exprInstr);
-            instr->setOperand(i, exprInstr);
-          }
-        }
+        // transform ConstantExpr in operands into new instructions
+        // for (int i = 0; i < instr->getNumOperands(); i++) {
+        //   Value *operand = instr->getOperand(i);
+        //   if (ConstantExpr *expr = dyn_cast<ConstantExpr>(operand)) {
+        //     Instruction *exprInstr = expr->getAsInstruction();
+        //     builder.Insert(exprInstr);
+        //     instr->setOperand(i, exprInstr);
+        //   }
+        // }
       }
     }
   }
+
+  return false;
+  // return NormalizeConstExpr::normalizeModule(module);
 }
+
 
 int main(int argc, char** argv) {
 
@@ -64,36 +77,16 @@ int main(int argc, char** argv) {
   module = parseIRFile(inputFileName, err, context);
 
   // print module
-  cout << "Before normalization: " << std::endl;
-  module->print(outs(), nullptr);
+  cout << "BEFORE NORMALIZATION: " << std::endl;
+  // module->print(outs(), nullptr);
 
-  preprocess_constant_expr(&(*module));
+  // NormalizeConstExpr *a;
+  // a->runOnModule(*module);
 
-  cout << "After normalization: " << std::endl;
-  module->print(outs(), nullptr);
+  // normalizeModule(*module);
 
-  // for (auto it = module->getFunctionList().begin();
-  //      it != module->getFunctionList().end();
-  //      ++it) {
-  //   auto func = it;
-  //   outs() << "Function: " << func->getName().data() << "\n";
-  //   for (auto it2 = func->getBasicBlockList().begin();
-  //        it2 != func->getBasicBlockList().end();
-  //        ++it2) {
-  //     auto blk = it2;
-  //     cout << "\n";
-  //     for (auto it3 = blk->begin(); it3 != blk->end(); ++it3) {
-  //       auto instr = it3;
-  //       outs() << "  inst data: " << *instr << "\n";
-  //       outs() << "  inst name: " << instr->getName().data() << "\n";
-  //       outs() << "  inst op: "
-  //                    << instr->getOperand(0)->getName()
-  //                    << "\n";
-  //       if (MDNode *N = instr->getMetadata("dbg")) {  // Here I is an LLVM instruction
-  //         outs() << "          has metadata\n";
-  //       }
-  //     }
-  //   }
-  // }
+  // cout << "AFTER NORMALIZATION: " << std::endl;
+  // module->print(outs(), nullptr);
 
+  return 1;
 }
