6a37bf78a2f6271dc47733fd3caf534ac8eaffd6
trungtq <taquangtrungvn@gmail.com>
2020-07-08 17:36:33 +0800

    restructure argument parsing

--- src/normalizer.cpp
+++ src/normalizer.cpp
@@ -7,12 +7,59 @@
 #include "llvm/Support/SourceMgr.h"
 #include "llvm/Support/raw_ostream.h"
 
+// #include "cxxopts/cxxopts.hpp"
 #include "cxxopts/cxxopts.hpp"
+
 #include "NormalizeConstExpr.h"
 
 using namespace std;
 using namespace llvm;
 using namespace discover;
+
+typedef struct Arguments {
+  string inputFile;
+  string outputFile;
+  bool debugging;
+} Arguments;
+
+Arguments parseArguments(int argc, char** argv) {
+  Arguments args;
+
+  // Parse arguments
+  cxxopts::Options options("LLVM-Normalizer", "One line description of Discover");
+  options.add_options()
+    ("input", "Input file", cxxopts::value<std::string>())
+    ("output", "Output file", cxxopts::value<std::string>())
+    ("debug", "Enable debugging")
+    ;
+
+  // parse arguments by positional and flags
+  options.parse_positional({"input"});
+  auto parsedOptions = options.parse(argc, argv);
+
+  // get input file
+  string inputFile = parsedOptions["input"].as<std::string>();
+  if (inputFile.empty()) {
+    cerr << "Input file not found";
+    exit(1);
+  }
+  args.inputFile = inputFile;
+  cout << "Input File: " << inputFile << std::endl;
+
+  // get output file
+  std::string outputFile;
+  if (parsedOptions.count("output"))
+    outputFile = parsedOptions["output"].as<std::string>();
+  if (!outputFile.empty()) {
+    args.outputFile = outputFile;
+    cout << "Output File: " << outputFile << std::endl;
+  }
+
+  // get debugging flag
+  args.debugging = parsedOptions["debug"].as<bool>();
+
+  return args;
+}
 
 bool normalizeModule(Module& module) {
   return NormalizeConstExpr::normalizeModule(module);
@@ -20,47 +67,42 @@ bool normalizeModule(Module& module) {
 
 
 int main(int argc, char** argv) {
-
   cout << "Discover-Llvm Normalizer" << std::endl;
 
-  cxxopts::Options options("LLVM-Normalizer", "One line description of Discover");
+  Arguments args = parseArguments(argc, argv);
+  string inputFile = args.inputFile;
+  string outputFile = args.outputFile;
+  bool debugging = args.debugging;
 
-  options.add_options()
-    ("out", "Output File", cxxopts::value<std::string>()) ;
-
-  auto parsedOptions = options.parse(argc, argv);
-
-  std::string outputFile;
-  if (parsedOptions.count("out"))
-    outputFile = parsedOptions["out"].as<std::string>();
-
-  cout << "Output File: " << outputFile << std::endl;
-
+  // process bitcode
   unique_ptr<Module> module;
   SMDiagnostic err;
   static LLVMContext context;
 
-  string inputFileName = *(argv+1);
-  cout << "Input File: " << inputFileName << std::endl;
+  module = parseIRFile(inputFile, err, context);
 
-  module = parseIRFile(inputFileName, err, context);
+  if (debugging) {
+    cout << "===============================\n"
+         << "BEFORE NORMALIZATION: " << std::endl;
+    module->print(outs(), nullptr);
+  }
 
-  // print module
-  cout << "===============================\n"
-       << "BEFORE NORMALIZATION: " << std::endl;
-  module->print(outs(), nullptr);
-
+  // normalize module
   normalizeModule(*module);
 
-  std::error_code EC;
-  raw_fd_ostream OS("module", EC, llvm::sys::fs::F_None);
+  if (debugging) {
+    cout << "===============================\n"
+         << "AFTER NORMALIZATION: " << std::endl;
+    module->print(outs(), nullptr);
+  }
 
-  WriteBitcodeToFile(*module, OS);
-  OS.flush();
-
-  cout << "===============================\n"
-       << "AFTER NORMALIZATION: " << std::endl;
-  module->print(outs(), nullptr);
+  // write output
+  if (!outputFile.empty()) {
+    std::error_code EC;
+    raw_fd_ostream OS(outputFile, EC, llvm::sys::fs::F_None);
+    WriteBitcodeToFile(*module, OS);
+    OS.flush();
+  }
 
   return 1;
 }
