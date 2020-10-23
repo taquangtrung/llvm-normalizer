baa77e05ec7bafe12ce12beb1baecbc640e739e6
trungtq <taquangtrungvn@gmail.com>
2020-10-24 01:30:35 +0800

    prepare to implement InlineFunction

---
+++ src/InlineFunction.h
@@ -0,0 +1,37 @@
+#include <iostream>
+
+#include "llvm/IR/Type.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/PassManager.h"
+
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
+using namespace std;
+using namespace llvm;
+
+using GlobalListType = SymbolTableList<GlobalVariable>;
+using FunctionListType = SymbolTableList<Function>;
+using BasicBlockListType = SymbolTableList<BasicBlock>;
+
+namespace discover {
+
+struct InlineFunction : public ModulePass {
+  static char ID;
+  static bool normalizeModule(Module &M);
+
+  void handleGlobals(Module &M);
+  void handleFunctions(Module &M);
+  void handleInstr(IRBuilder<> builder, Instruction* instr);
+
+  InlineFunction() : ModulePass(ID) {}
+
+  virtual bool runOnModule(Module &M) override;
+};
+
+} // namespace discover
