193e5d25f242f1f2123c6922342e4787d080e9bc
trungtq <taquangtrungvn@gmail.com>
2021-01-30 00:08:43 +0800

    update

---
+++ src/ElimCommonGEP.h
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include "llvm/IR/Type.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/PassManager.h"
+
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
+#include "Debug.h"
+#include "Common.h"
+
+using namespace std;
+using namespace llvm;
+
+namespace discover {
+
+struct ElimCommonGEP : public ModulePass {
+  static char ID;
+  static bool normalizeModule(Module &M);
+
+  GetElementPtrInst* findGEPOfSameElement(GetElementPtrInst *instr);
+  bool processFunction(Function *func);
+  void handleFunctions(Module &M);
+
+  ElimCommonGEP() : ModulePass(ID) {}
+
+  virtual bool runOnModule(Module &M) override;
+};
+
+} // namespace discover
