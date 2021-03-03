49ad68c0ea45a52500bf853439822eb63f2a3963
trungtq <taquangtrungvn@gmail.com>
2021-03-03 18:27:05 +0800

    working on SplitCallInstrs

---
+++ src/SplitCallInstrs.cpp
@@ -0,0 +1,46 @@
+#include "SplitCallInstrs.h"
+
+using namespace discover;
+using namespace llvm;
+
+using GEPInstList = std::vector<GetElementPtrInst*>;
+
+/*
+ * This pass implement a callsplit pass, which is similar to LLVM,
+ * but splitting is allowed on non-constant arguments of function calls.
+ */
+
+char SplitCallInstrs::ID = 0;
+
+
+/*
+ * Entry function for this FunctionPass, can be used by llvm-opt
+ */
+bool SplitCallInstrs::runOnFunction(Function &F) {
+  std::vector<GEPInstList> allGEPList = findCombinableGEPList(F);
+  combineGEPInstructions(F, allGEPList);
+  return true;
+}
+
+/*
+ * Static function, used by this normalizer
+ */
+bool SplitCallInstrs::normalizeFunction(Function &F) {
+  debug() << "\n=========================================\n"
+          << "Combining GEP Instruction in function: "
+          << F.getName() << "\n";
+
+  SplitCallInstrs pass;
+  return pass.runOnFunction(F);
+}
+
+static RegisterPass<SplitCallInstrs> X("SplitCallInstrs",
+                                  "SplitCallInstrs Pass",
+                                  false /* Only looks at CFG */,
+                                  false /* Analysis Pass */);
+
+static RegisterStandardPasses Y(PassManagerBuilder::EP_EarlyAsPossible,
+                                [](const PassManagerBuilder &Builder,
+                                   legacy::PassManagerBase &PM) {
+                                  PM.add(new SplitCallInstrs());
+                                });
---
+++ src/SplitCallInstrs.h
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#include "llvm/IR/Type.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/PassManager.h"
+
+#include "llvm/Pass.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
+#include "Debug.h"
+#include "Common.h"
+
+using namespace std;
+using namespace llvm;
+
+namespace discover {
+
+struct SplitCallInstrs : public FunctionPass {
+  static char ID;
+  static bool normalizeFunction(Function &F);
+
+  SplitCallInstrs() : FunctionPass(ID) {}
+
+  virtual bool runOnFunction(Function &F) override;
+};
+
+} // namespace discover
